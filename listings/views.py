e850a05adc91ca0f4471d5f23f0a0e5c8bf2aef6
Nokukhanya-Mitane <nokukhanyamitane@gmail.com>
2026-01-12 08:01:41 +0200

    Create views.py

---
+++ listings/views.py
@@ -0,0 +1,13 @@
+from rest_framework.viewsets import ModelViewSet
+from .models import Listing, Booking
+from .serializers import ListingSerializer, BookingSerializer
+
+
+class ListingViewSet(ModelViewSet):
+    queryset = Listing.objects.all()
+    serializer_class = ListingSerializer
+
+
+class BookingViewSet(ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
